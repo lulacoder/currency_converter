0a4c30aef41b9afa54fad63e6c34834810d4c985
leul <leultesfaye0755@gmail.com>
2025-12-14 22:51:45 -0800

    created input box with button more styling to come

--- lib/currency_converter_material_page.dart
+++ lib/currency_converter_material_page.dart
@@ -5,8 +5,44 @@ class CurrencyConverterMaterialPage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    // TODO: implement build
-    return const Scaffold(body: Center(child: Text("hello world")));
-   
+    return Scaffold(
+      backgroundColor: Color.fromRGBO(0, 0, 0, 1),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Text(
+              "0",
+              style: TextStyle(
+                fontSize: 55,
+                fontWeight: FontWeight.bold,
+                color: Color.fromARGB(255, 255, 255, 255),
+              ),
+            ),
+            Padding(
+              padding: EdgeInsets.all(8.0),
+              child: const TextField(
+                style: TextStyle(color: Colors.white),
+                decoration: InputDecoration(
+                  hintText: "Please enter the amount in USD",
+                  hintStyle: TextStyle(color: Colors.white60),
+                  prefixIcon: Icon(Icons.monetization_on),
+                  prefixIconColor: Colors.white,
+                  filled: true,
+                  fillColor: Colors.white,
+                  focusedBorder: OutlineInputBorder(),
+                ),
+              ),
+            ),
+            TextButton(
+              onPressed: () {
+                print('what up boy');
+              },
+              child: Text('hello world'),
+            ),
+          ],
+        ),
+      ),
+    );
   }
 }
